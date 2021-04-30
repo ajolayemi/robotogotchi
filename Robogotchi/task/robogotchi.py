14987b32a14fdda247c5473e79227f24ac1b6402
jolayemi_local <ajolayemi82@gmail.com>
2021-04-30 21:08:25 +0200

    Completed stage 2

--- Robogotchi/task/robogotchi.py
+++ Robogotchi/task/robogotchi.py
@@ -1,10 +1,45 @@
 import random
+import sys
 
 MIN = 0
 MAX = 1000000
 
+RULES = {'rock': 'scissors',
+         'paper': 'rock',
+         'scissors': 'paper'}
 
-def play_game():
+
+def play_rock_game():
+    stats = {"User": 0,
+             "Robot": 0,
+             "Draws": 0}
+    while True:
+        user_weapon = input('What is your move?\n')
+        robot_weapon = random.choice(list(RULES.keys()))
+        if user_weapon == 'exit game':
+            print()
+            print(f"You won: {stats.get('User')},\n"
+                  f"The robot won: {stats.get('Robot')}, \n"
+                  f"Draws: {stats.get('Draws')}.")
+            break
+        elif user_weapon not in RULES.keys():
+            print('No such option! Try again!')
+
+        else:
+            print(f'The robot chose {robot_weapon}')
+            if user_weapon == robot_weapon:
+                print("It's a draw!\n")
+                stats['Draws'] += 1
+            elif robot_weapon == RULES.get(user_weapon):
+                print('You won!\n')
+                stats['User'] += 1
+
+            elif user_weapon == RULES.get(robot_weapon):
+                print('The robot won!\n')
+                stats['Robot'] += 1
+
+
+def play_number_game():
     stats = {"User": 0,
              "Robot": 0,
              "Draws": 0}
@@ -44,5 +79,24 @@ def play_game():
             print('A string is not a valid input!')
 
 
+def main():
+    available_games = {'Numbers': play_number_game,
+                       'Rock-paper-scissors': play_rock_game}
+    while True:
+        game_choice = input("Which game would you like to play?\n")
+        if game_choice in available_games.keys():
+            available_games.get(game_choice)()
+            sys.exit()
+        else:
+            while True:
+                if game_choice not in available_games.keys():
+                    game_choice = input('Please choose a valid option: Numbers or Rock-paper-scissors?\n')
+                else:
+                    available_games.get(game_choice)()
+                    sys.exit()
+
+
+
+
 if __name__ == '__main__':
-    play_game()
+    main()
